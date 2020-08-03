5ff59d8dacdf78dce4e7d4bd9fe33e6ca2b246f5
Manu Mital <mitalvt@gmail.com>
2020-08-03 15:13:13 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        // Inject IEmployeeRepository using Constructor Injection
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        // Retrieve employee name and return
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Email;
+        }
+
+        public ViewResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            ViewData["PageTitle"] = "Employee Details";
+            ViewData["Employee"] = model;
+            return View();
+        }
+
+    }
+}
